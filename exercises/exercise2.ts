d9db07a36539d1be10aff9da5decce3afb42f7ad
Clinton Williams <clintonwilliams007@gmail.com>
2024-05-22 17:57:21 -0400

    created gradebook logic

--- exercises/exercise2.ts
+++ exercises/exercise2.ts
@@ -1,14 +1,38 @@
-function getAverage(scores: number[]) {
+export function getAverage(scores: number[]) {
   let sum = 0;
-
   for (const score of scores) {
     sum += score;
   }
   return sum / scores.length;
 }
 
-export default function exercise2(){
- 
-getAverage([34, 43, 69, 49, 39]);
 
-};
+function getGrade(score: number) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+function hasPassingGrade(score: number) {
+  return getGrade(score) !== "F";
+}
+
+function studentMsg(totalScores: number[], studentScore: number) {
+  const averageScores = getAverage(totalScores);
+  const yourGrade = getGrade(studentScore);
+  const passMsg = hasPassingGrade(studentScore) ? "You passed the course." : "You failed the course.";
+  
+  return `Class average: ${averageScores}. Your grade: ${yourGrade}. ${passMsg}`;
+}
+
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
